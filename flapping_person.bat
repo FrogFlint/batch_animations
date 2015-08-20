9149383a4f68b3e441a89b68e1cf3a644ed54ce8
FrogFlint <james.flint@outlook.com>
2015-08-20 16:00:48 -0400

    first animation
    Tap any key to cycle through the frames, or hold to make him fly!

---
+++ flapping_person.bat
@@ -0,0 +1,73 @@
+@echo off
+pause
+color 0a
+mode 1000
+
+:matrix
+
+echo           _____
+echo          /_____\
+echo          ! *_* !
+echo         __\   /__
+echo        /         \
+echo       / /!     !\ \
+echo      /_/ !     ! \_\
+echo          !_____!
+echo          /  _  \
+echo         /  / \  \
+echo       _/  /   \  \_
+echo      !___/     \___!
+echo    /-----------------\
+echo    \-----------------/
+pause>nul
+cls
+echo           _____
+echo          /_____\    
+echo          ! *_* !    
+echo     ______\   /______
+echo     \____       ____/
+echo          !     !   
+echo          !     !    
+echo          !_____!
+echo          /  _  \
+echo         /  / \  \
+echo       _/  /   \  \_
+echo      !___/     \___!
+echo    /-----------------\
+echo    \-----------------/
+pause>nul
+cls
+echo           _____
+echo      __  /_____\  __
+echo      \ \ ! *_* ! / /
+echo       \ \_\   /_/ /
+echo        \_       _/
+echo          !     !   
+echo          !     !    
+echo          !_____!
+echo          /  _  \
+echo         /  / \  \
+echo       _/  /   \  \_
+echo      !___/     \___!
+echo    /-----------------\
+echo    \-----------------/
+pause>nul
+cls
+echo           _____
+echo          /_____\    
+echo          ! *_* !    
+echo      _____\   /_____
+echo     /____       ____\
+echo          !     !   
+echo          !     !    
+echo          !_____!
+echo          /  _  \
+echo         /  / \  \
+echo       _/  /   \  \_
+echo      !___/     \___!
+echo    /-----------------\
+echo    \-----------------/
+pause>nul
+cls
+
+goto matrix
