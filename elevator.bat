d096db5d79c297b5214bc7a45469d5a33b007759
FrogFlint <james.flint@outlook.com>
2015-08-24 15:10:23 -0400

    second animation
    Press and hold any key to make it go up.

---
+++ elevator.bat
@@ -0,0 +1,538 @@
+echo off
+pause
+color 0A
+mode 1000
+
+:elevator
+
+cls
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+pause>nul
+
+cls
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+pause>nul
+
+cls
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+pause>nul
+
+cls
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+pause>nul
+
+cls
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+pause>nul
+
+cls
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+pause>nul
+
+cls
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+pause>nul
+
+cls
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+pause>nul
+
+cls
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+pause>nul
+
+cls
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo      !
+echo    -----
+echo    !   !
+echo    !   !
+echo    -----
+echo      !
+echo      !
+echo      !
+pause>nul
+
+goto elevator
