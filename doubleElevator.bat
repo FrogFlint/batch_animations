82bcb0abf74962390e6322c3063b082ccf0988bf
FrogFlint <james.flint@outlook.com>
2015-08-25 15:53:12 -0400

    second commit v1.1
    Two elevators move in opposite directions. The guy in the left elevator flaps his arms as it goes up.

---
+++ doubleElevator.bat
@@ -0,0 +1,556 @@
+echo off
+pause
+color 0A
+mode 1000
+
+:elevator
+
+cls
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    !\O/!     ----- 
+echo    ! T !     !   ! 
+echo    -----     !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    !\O/!     ----- 
+echo    ! T !     !   ! 
+echo    -----     !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    !_O_!     ----- 
+echo    ! T !     !   ! 
+echo    -----     !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    ! O !     ----- 
+echo    !/T\!     !   ! 
+echo    -----     !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    !_O_!     ----- 
+echo    ! T !     !   ! 
+echo    -----     !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+pause>nul
+
+
+
+cls
+echo      !         !   
+echo    -----       !   
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----     ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    !_O_!       !   
+echo    ! T !       !   
+echo    -----     ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    ! O !       !   
+echo    !/T\!       !   
+echo    -----     ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    !_O_!       !   
+echo    ! T !       !   
+echo    -----     ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----     ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+pause>nul
+
+
+
+cls
+echo    -----       !   
+echo    !_O_!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo    -----       !   
+echo    ! O !       !   
+echo    !/T\!       !   
+echo    -----       !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo    -----       !   
+echo    !_O_!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo    -----       !   
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo    -----       !   
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+pause>nul
+
+
+
+cls
+echo    ! O !       !   
+echo    !/T\!       !   
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo    -----     ----- 
+echo    !_O_!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo    -----     ----- 
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo    -----     ----- 
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo    -----     ----- 
+echo    !_O_!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo    -----     ----- 
+pause>nul
+
+
+
+cls
+echo    ! T !     ----- 
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo    -----     !   ! 
+echo    !\O/!     !/T\! 
+echo    ! T !     ----- 
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo    -----     !   ! 
+echo    !\O/!     !/T\! 
+echo    ! T !     ----- 
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo    -----     !   ! 
+echo    !_O_!     !/T\! 
+echo    ! T !     ----- 
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo    -----     !   ! 
+echo    ! O !     !/T\! 
+echo    !/T\!     ----- 
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo    -----     !   ! 
+echo    !_O_!     !/T\! 
+pause>nul
+
+
+
+cls
+echo    -----     !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    !\O/!     ----- 
+echo    ! T !     !   ! 
+echo    -----     !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    !_O_!     ----- 
+echo    ! T !     !   ! 
+echo    -----     !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    ! O !     ----- 
+echo    !/T\!     !   ! 
+echo    -----     !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    !_O_!     ----- 
+echo    ! T !     !   ! 
+echo    -----     !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    !\O/!     ----- 
+echo    ! T !     !   ! 
+pause>nul
+
+
+
+cls
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----     ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----     ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----     ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----     ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo    -----       !   
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----     ----- 
+pause>nul
+
+
+
+cls
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo    -----       !   
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo    -----       !   
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo    -----       !   
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo    -----       !   
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo      !       ----- 
+echo      !         !   
+echo    -----       !   
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+pause>nul
+
+
+
+cls
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo    -----     ----- 
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo    -----     ----- 
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo    -----     ----- 
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo    -----     ----- 
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo      !       !   ! 
+echo      !       !/T\! 
+echo    -----     ----- 
+echo    !\O/!       !   
+echo    ! T !       !   
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+pause>nul
+
+
+
+cls
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo    -----     !   ! 
+echo    !\O/!     !/T\! 
+echo    ! T !     ----- 
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo    -----     !   ! 
+echo    !\O/!     !/T\! 
+echo    ! T !     ----- 
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo    -----     !   ! 
+echo    !\O/!     !/T\! 
+echo    ! T !     ----- 
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo    -----     !   ! 
+echo    !\O/!     !/T\! 
+echo    ! T !     ----- 
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+echo      !       ----- 
+echo    -----     !   ! 
+echo    !\O/!     !/T\! 
+echo    ! T !     ----- 
+echo    -----       !   
+echo      !         !   
+echo      !         !   
+echo      !         !   
+pause>nul
+
+goto elevator
